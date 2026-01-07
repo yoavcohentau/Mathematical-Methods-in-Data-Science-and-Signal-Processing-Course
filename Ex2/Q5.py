fb96c8ed88dcd8ae644ea71e8b27666788ab1710
Yoav Cohen <yoavc4@mail.tau.ac.il>
2026-01-07 21:35:43 +0200

    use the algorithm exactly the same to the slides

--- Ex2/Q5.py
+++ Ex2/Q5.py
@@ -7,21 +7,23 @@ from sklearn.decomposition import PCA
 import itertools
 
 
-# ------------------------
-# Load data
-# ------------------------
-X = np.loadtxt("./Q5_data/data.csv", delimiter=",")
-I_true = np.loadtxt("./Q5_data/clusters.csv", delimiter=",")
+# ------ Error metrics ------
+def test_performance(true_labels, pred_labels, k=3):
+    I_true = labels_to_indicator(true_labels, k)
+    I_pred = labels_to_indicator(pred_labels, k)
 
-n = X.shape[0]
-k = 3
-true_labels = np.argmax(I_true, axis=1)
+    frob_err = []
+    acc = []
+    for p in itertools.permutations(range(k)):
+        frob_err.append(0.5 * np.linalg.norm(I_true - I_pred[:, p], 'fro'))
+        mapped = np.array([p[l] for l in pred_labels])
+        acc.append(np.mean(mapped != true_labels))
+    idx_min = np.argmin(frob_err)
+
+    return frob_err[idx_min], acc[idx_min]
 
 
-# ------------------------
-# Error metrics
-# ------------------------
-def misclassification_rate(true_labels, pred_labels):
+def calc_accuracy_rate(true_labels, pred_labels):
     best = np.inf
     for p in itertools.permutations(range(k)):
         mapped = np.array([p[l] for l in pred_labels])
@@ -36,6 +38,7 @@ def frobenius_error(I_true, I_pred):
     return min(errs)
 
 
+# ------ utils ------
 def labels_to_indicator(labels, k):
     I = np.zeros((len(labels), k))
     for i, l in enumerate(labels):
@@ -43,23 +46,33 @@ def labels_to_indicator(labels, k):
     return I
 
 
-# ------------------------
-# k-means
-# ------------------------
+# ------ Simulation ------
+
+# Load data
+X = np.loadtxt("./Q5_data/data.csv", delimiter=",")
+I_true = np.loadtxt("./Q5_data/clusters.csv", delimiter=",")
+true_labels = np.argmax(I_true, axis=1)
+
+# parameters
+n = X.shape[0]
+k = 3
+
+
+# -- (i) pure k-means
 _, labels_km = kmeans2(X, k, minit='++', iter=50)
 I_km = labels_to_indicator(labels_km, k)
 
-err_km_rate = misclassification_rate(true_labels, labels_km)
+err_km_rate = calc_accuracy_rate(true_labels, labels_km)
 err_km_frob = frobenius_error(I_true, I_km)
 
 print("k-means misclassification rate:", 1 - err_km_rate)
 print("k-means Frobenius error:", err_km_frob)
 
-# ------------------------
-# Spectral clustering
-# ------------------------
+
+# -- (ii) spectral clustering
 # Gaussian kernel (local scale)
-sigma = np.percentile(cdist(X, X), 10) / 10
+# sigma = np.percentile(cdist(X, X), 10) / 10
+sigma = np.percentile(cdist(X, X), 20) / 10
 # sigma = 1.2561588877651955
 W = np.exp(-cdist(X, X)**2 / (2 * sigma**2))
 np.fill_diagonal(W, 0)
@@ -72,11 +85,12 @@ L_sym = np.eye(n) - D_inv_sqrt @ W @ D_inv_sqrt
 
 # Eigen-decomposition
 eigvals, eigvecs = eigh(L_sym)
-U = eigvecs[:, 1:k+1]     # skip trivial eigenvector
-# U = eigvecs[:, :k]
+# U = eigvecs[:, 1:k+1]     # skip trivial eigenvector
+V = eigvecs[:, 1:k]          # v_2,...,v_k
+U = D_inv_sqrt @ V
 
 # Row normalization (CRITICAL)
-U = U / np.linalg.norm(U, axis=1, keepdims=True)
+# U = U / np.linalg.norm(U, axis=1, keepdims=True)
 
 # k-means on spectral embedding
 # err_spec_frob = np.inf
@@ -94,7 +108,7 @@ for _ in range(50):
     _, labels_spec = kmeans2(U, k, minit='++', iter=50)
     I_spec = labels_to_indicator(labels_spec, k)
 
-    err_spec_rate = misclassification_rate(true_labels, labels_spec)
+    err_spec_rate = calc_accuracy_rate(true_labels, labels_spec)
     err_spec_frob = frobenius_error(I_true, I_spec)
 
     if err_spec_frob < err_spec_frob_min:
@@ -115,156 +129,23 @@ print("Spectral Frobenius error:", err_spec_frob)
 pca = PCA(n_components=2)
 X_pca = pca.fit_transform(X)
 
-plt.figure(figsize=(12, 5))
+plt.figure(figsize=(12, 4))
 
-plt.subplot(1, 2, 1)
+plt.subplot(1, 3, 1)
+plt.scatter(X_pca[:, 0], X_pca[:, 1], c=true_labels, cmap='tab10', s=15)
+plt.title("ground-truth clustering (PCA view)")
+plt.axis("equal")
+
+plt.subplot(1, 3, 2)
 plt.scatter(X_pca[:, 0], X_pca[:, 1], c=labels_km, cmap='tab10', s=15)
 plt.title("k-means clustering (PCA view)")
 plt.axis("equal")
 
-plt.subplot(1, 2, 2)
+plt.subplot(1, 3, 3)
 plt.scatter(X_pca[:, 0], X_pca[:, 1], c=labels_spec, cmap='tab10', s=15)
 plt.title("Spectral clustering (PCA view)")
 plt.axis("equal")
 
+plt.suptitle('Data in PCA view')
 plt.tight_layout()
 plt.show()
-
-
-
-# import numpy as np
-# from scipy.cluster.vq import kmeans2
-# from scipy.spatial.distance import cdist
-# from numpy.linalg import eigh
-# import itertools
-#
-#
-# DATA_FILE_PATH = rf'./Q5_data/data.csv'
-# CLUSTER_FILE_PATH = rf'./Q5_data/clusters.csv'
-#
-# X = np.loadtxt(DATA_FILE_PATH, delimiter=",")
-# I_true = np.loadtxt(CLUSTER_FILE_PATH, delimiter=",")
-#
-#
-# n = X.shape[0]
-# k = 3
-#
-#
-# def clustering_error(I_true, I_pred):
-#     errs = []
-#     errs_counter = []
-#     for p in itertools.permutations(range(3)):
-#         errs.append(0.5 * np.linalg.norm(I_true - I_pred[:, p], 'fro'))
-#         errs_counter.append(0.5*np.count_nonzero(I_true - I_pred[:, p]))
-#     return min(errs)
-#
-#
-# # 5-(i)
-# centroids, labels = kmeans2(
-#     X,
-#     3,
-#     minit='++',
-#     iter=50
-# )
-#
-# I_kmeans = np.zeros((n, 3))
-# for i in range(n):
-#     I_kmeans[i, labels[i]] = 1
-#
-# err_kmeans = clustering_error(I_true, I_kmeans)
-# print("k-means error:", err_kmeans)
-#
-#
-# # 5-(i)
-# sigma = np.median(cdist(X, X))
-# W = np.exp(-cdist(X, X)**2 / (2*sigma**2))
-# np.fill_diagonal(W, 0)
-#
-#
-# D = np.diag(W.sum(axis=1))
-# L = D - W
-#
-#
-# eigvals, eigvecs = eigh(L)
-# U = eigvecs[:, :3]
-#
-#
-# centroids_spec, labels_spec = kmeans2(
-#     U,
-#     3,
-#     minit='++',
-#     iter=50
-# )
-#
-# I_spec = np.zeros((n, 3))
-# for i in range(n):
-#     I_spec[i, labels_spec[i]] = 1
-#
-# err_spec = clustering_error(I_true, I_spec)
-# print("Spectral clustering error:", err_spec)
-#
-#
-#
-#
-# # import numpy as np
-# # from sklearn.cluster import KMeans
-# # from scipy.spatial.distance import cdist
-# # from numpy.linalg import eigh
-# # import itertools
-# #
-# #
-# # DATA_FILE_PATH = rf'./Q5_data/data.csv'
-# # CLUSTER_FILE_PATH = rf'./Q5_data/clusters.csv'
-# #
-# # X = np.loadtxt(DATA_FILE_PATH, delimiter=",")
-# # I_true = np.loadtxt(CLUSTER_FILE_PATH, delimiter=",")
-# #
-# #
-# # n = X.shape[0]
-# # k = 3
-# #
-# #
-# # def clustering_error(I_true, I_pred):
-# #     errs = []
-# #     for p in itertools.permutations(range(3)):
-# #         errs.append(0.5 * np.linalg.norm(I_true - I_pred[:, p], 'fro'))
-# #     return min(errs)
-# #
-# #
-# # # 5-(i)
-# # kmeans = KMeans(n_clusters=3, n_init=20)
-# # labels = kmeans.fit_predict(X)
-# #
-# # I_kmeans = np.zeros((n, 3))
-# # for i in range(n):
-# #     I_kmeans[i, labels[i]] = 1
-# #
-# # err_kmeans = clustering_error(I_true, I_kmeans)
-# # print("k-means error:", err_kmeans)
-# #
-# #
-# # # 5-(i)
-# # sigma = np.median(cdist(X, X))
-# # W = np.exp(-cdist(X, X)**2 / (2*sigma**2))
-# # np.fill_diagonal(W, 0)
-# #
-# #
-# # D = np.diag(W.sum(axis=1))
-# # L = D - W
-# #
-# #
-# # eigvals, eigvecs = eigh(L)
-# # U = eigvecs[:, :3]
-# #
-# #
-# # kmeans = KMeans(n_clusters=3, n_init=20)
-# # labels_spec = kmeans.fit_predict(U)
-# #
-# # I_spec = np.zeros((n, 3))
-# # for i in range(n):
-# #     I_spec[i, labels_spec[i]] = 1
-# #
-# # err_spec = clustering_error(I_true, I_spec)
-# # print("Spectral clustering error:", err_spec)
-# #
-# #
